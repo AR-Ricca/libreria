286ad8d17f2d7f4e7b10065d1c37c688bdc6a1fd
AR-Ricca <alericca10093@gmail.com>
2024-09-30 13:46:25 +0200

    libreria

---
+++ classi/classi/Libro.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classi
+{
+    class Libro
+    {
+       public string Titolo { get; set; }
+        public string Autore { get; set; }
+
+        public int prezzo { get; set; }
+
+        public decimal  pagine { get; set; }
+    }
+}
---
+++ classi/classi/Program.cs
@@ -0,0 +1,27 @@
+﻿using classi;
+
+Console.WriteLine("Gestione Biblioteca");
+
+Console.WriteLine("inserimento Libri");
+
+
+bool continua = true;
+while (continua)
+{         
+    Libro libro = new Libro();
+
+    Console.WriteLine("Inserisci il titolo");
+    string titolo = Console.ReadLine();
+    Console.WriteLine("Inserisci L' autore");
+    string autore = Console.ReadLine();
+    Console.WriteLine("Inserisci il numero di pagine");
+    string pagine = Console.ReadLine();
+    Console.WriteLine("Inserisci il prezzo");
+    decimal prezzo = decimal.Parse(Console.ReadLine());
+
+
+    Console.WriteLine("vuoi inserire un altro libro?  (S/N)");
+    string risposta = Console.ReadLine();
+    if (risposta == "N")  continua = false;
+
+}
